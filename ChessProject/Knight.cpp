1bd9cd8a5e3962e6b3061ae37ee0a62b8a1a15fa
MrPigeon16 <ilaybinyamin63@gmail.com>
2024-12-22 19:38:22 +0200

    added Knight

---
+++ ChessProject/Knight.cpp
@@ -0,0 +1,41 @@
+#include "Knight.h"
+
+
+
+Knight::Knight(const std::string name, const std::string type, const std::string position)
+	:Piece(name, type, position)
+{
+
+}
+
+
+
+bool Knight::isLegitMove(const std::string position)
+{
+	std::string currentPosition = getPosition();
+	int left_right_move_squre = std::abs(currentPosition[0] - position[0]);
+	int up_down_move_squre = std::abs(currentPosition[1] - position[1]);
+	if ((left_right_move_squre == 2 && up_down_move_squre == 1) || (up_down_move_squre == 2 && left_right_move_squre == 1))
+	{
+		return true;
+	}
+	return false;
+}
+
+std::string Knight::move(const std::string newPosition)
+{
+
+	if (isLegitMove(newPosition))
+	{
+		setPosition(newPosition);
+		return std::to_string(VALID_MOVE);
+	}
+	return std::to_string(INVALID_ILLEGAL_MOVE);
+}
+
+
+Knight::~Knight()
+{
+}
+
+
--- ChessProject/Knight.h
+++ ChessProject/Knight.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Piece.h"
+
+class Knight : public Piece
+{
+	Knight(const std::string name, const std::string type, const std::string position);
+	~Knight();
+
+	virtual bool isLegitMove(const std::string position) override;
+	virtual std::string move(const std::string newPosition) override;
+
+
+};
